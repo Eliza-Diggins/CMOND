6e6c60cf66efce2305b522040e403fd5bc016cbd
Nathan Diggins <nathandigginsbms@gmail.com>
2022-06-21 10:28:47 -0600

    First commit to master.

---
+++ Modules/Clusters.py
@@ -0,0 +1,303 @@
+"""
+
+    Galaxy Cluster Models and Analysis
+        Written by: Eliza Diggins
+
+
+"""
+
+### IMPORTS ###
+import numpy as np
+import scipy as sci
+import matplotlib.pyplot as plt
+import logging as log
+import sympy as sym
+import astropy.units as u
+import astropy.constants as consts
+import pandas as pd
+import os
+
+######################################
+#                                    #
+#          Unit Management           #
+#                                    #
+######################################
+_CMOND_base_units = {"m":u.kpc,
+                     "kg":u.solMass,
+                     "K":u.K,
+                     "s":u.s,
+                     "E":u.keV}
+######################################
+#                                    #
+#          State Variables           #
+#                                    #
+######################################
+_mu = 1.624  # The constant used to convert n_pn_e(r) to gas mass.
+_nu = 0.5954 # The metalicity parameter of the universe.
+_a0 = 1.2e-11 *(u.m/u.s**2) # The MOND acceleration constant.
+
+######################################
+#                                    #
+#         Class Definitions          #
+#                                    #
+######################################
+
+def standard_interpolation_function(x):
+    return x/(1+x)
+
+class Cluster():
+    def __init__(self,name=None,sphS=True):
+        """
+        Defines a given cluster with a choice of model.
+        :param name: The name of the cluster. None will produce a random cluster name.
+        :param sphS: True if the cluster is to be treated as spherically symmetric. False to use a more complex coordinate system. TODO: implementation.
+        """
+
+        # Intro debugging
+        log.debug("CMOND:Clusters:Cluster:DEBUG: Creating a cluster with name <%s>"%name)
+
+        # Naming the cluster
+
+        if name:
+            self.name = name
+        else: # Generating random name
+            self.name = "CL"+str(np.random.randint(0,99999))
+
+        # Adding key namespace parameters
+        self.r_min = None
+        self.r_500 = None
+        self._has_dynamical_mass_profile_newt = False
+        self._has_temp_profile = False
+        self._has_density_profile=False
+
+
+    def temp_fit_sym(self,params,model):
+        """
+        Creates a temperature fit as a function of radius for spherically symetric clusters
+        :param params: The parameters as a dictionary of values with astropy units.
+        :type params: dict[str,u.quantity.Quantity or float]
+        :param model: The type of the temperature fit. Options: ["VIKH"]
+        :type model: str
+        :return: sympy expression of the fit with the correct values of the parameters.
+        """
+        ### Handling params units ###
+
+        new_params = {param: convert_to_base_units(params[param]).value for param in params}
+        unit_params = {param: convert_to_base_units(params[param]).unit for param in params}
+        log.info("CMOND:Clusters:Cluster:temp_fit:INFO: Converted params %s to %s in base units." % (
+            params, new_params))
+
+        if model == "VIKH":
+            """
+            Using the Vihklinin 2006 paper fits for temperature: namely
+            
+            T(r) = T_0[((r/r_cool)+(T_min/T_0))/((r/r_cool)^a_cool + 1)] ((r/r_t)^-a/((r/r_t)^b+1)^(c/b))
+            """
+            # Defining the base expression
+            expression_string_literal = "T_0*(((r/r_cool)**a_cool+(T_min/T_0))/((r/r_cool)**a_cool + 1))*((r/r_t)**(-a)/((r/r_t)**b+1)**(c/b))"
+
+            # Converting to symbolic expression
+            expression = sym.sympify(expression_string_literal)
+
+
+            self.temp_profile = expression.subs([(sym.symbols(i),new_params[i]) for i in new_params]) # substitutes all of the parameters
+            self._has_temp_profile = True
+            self._temp_profile_output_units = unit_params["T_0"]
+
+    def density_fit_sym(self, params, model):
+        """
+        Creates a density fit as a function of radius for spherically symetric clusters
+        :param params: The parameters as a dictionary of values with astropy units.
+        :type params: dict[str,u.quantity.Quantity or float]
+        :param model: The type of the temperature fit. Options: ["VIKH"]
+        :type model: str
+        :return: sympy expression of the fit with the correct values of the parameters.
+        """
+        ### Handling params units ###
+        # We are working in united values
+        new_params = {param: convert_to_base_units(params[param]).value for param in params}
+        unit_params = {param: convert_to_base_units(params[param]).unit for param in params}
+        log.info("CMOND:Clusters:Cluster:temp_fit:INFO: Converted params %s to %s in base units." % (
+            params, new_params))
+
+        if model == "VIKH":
+            """
+            Using the Vihklinin 2006 paper fits for temperature: namely
+
+            n_en_p (r) = (n_0**2)*(((r/r_c)**(-a))/((1+(r/r_c)**2)**(3*beta-(a/2))))*(1/((1+(r/r_s))**(epsilon/gamma))) + ((n_0**2)/((1+(r/r_c2)**2)**(3*beta_2)))
+            """
+            # Defining the base expression
+            expression_string_literal =  '%s*m_p*((n_0**2)*(((r/r_c)**(-ALPHA))/((1+(r/r_c)**2)**(3*BETA-(ALPHA/2))))*(1/((1+(r/r_s)**(GAMMA))**(EPSILON/GAMMA))) + ((n_02**2)/((1+(r/r_c2)**2)**(3*BETA_2))))**(1/2)'%_mu
+
+            # Converting to symbolic expression
+            expression = sym.sympify(expression_string_literal)
+
+
+            self.density_profile = expression.subs(
+                [(sym.symbols(i), new_params[i]) for i in new_params])  # substitutes all of the parameters
+            self._has_density_profile = True
+
+            self._density_profile_output_units = (unit_params["m_p"]*unit_params["n_0"])
+
+    def dynamical_mass_fit_sym(self,gravitational_mode="NEWT",interpolation_function="x/(x+1)",nu=0.5954):
+        """
+        Constructs the dynamical mass of the system between the two given radii
+        :param gravitational_mode:
+        :param interpolation_function:
+        :return:
+        """
+        # Intro logging
+        log.info("CMOND:Clusters:Cluster:dynamical_mass_fit_sym:INFO: Fitting mass to cluster %s."%self.name)
+
+        # Sanitizing
+        if not self._has_density_profile or not self._has_temp_profile: # The required data are not there.
+            raise NotImplementedError("CMOND:Clusters:Cluster:dynamical_mass_fit_sym:ERROR: Cluster %s does not have sufficient models for this computation."%self.name)
+
+
+        # Creating the radial array
+        """
+        Completing the necessary computations to find the dynamical mass.
+            
+            Note: In the newtonian formalism,
+            
+            M(<r) = -k*r^2/G*nu*m_p [drho_g/dr*(T/rho) + dT/dr]
+        
+        """
+        ### Compute the derivatives
+        dTdr_sym = sym.diff(self.temp_profile,sym.Symbol("r"))
+        drhodr_sym = sym.diff(self.density_profile,sym.Symbol("r"))
+
+        ### Computing the coefficient
+        coeff = 1/(consts.G*nu*consts.m_p)
+        coeff_mond = 1/(consts.m_p*nu*_a0)
+
+        ### Handling units
+        output_units = self._temp_profile_output_units*_CMOND_base_units["m"]*coeff.unit
+
+        ### Managing modes
+        if gravitational_mode == "NEWT":
+            ### Symbolic mass function
+            self.dynamical_mass_profile_newt = -(sym.Symbol("r")**2)*coeff.value*(((self.temp_profile/self.density_profile)*(drhodr_sym))+(dTdr_sym))
+            self._has_dynamical_mass_profile_newt = True
+            self._dynamical_mass_profile_newt_units = output_units
+        if gravitational_mode == "MOND":
+            ### Symbolic mass function
+            self.dynamical_mass_profile_mond =-(sym.Symbol("r")**2)*coeff.value*(((self.temp_profile/self.density_profile)*(drhodr_sym))+(dTdr_sym))*(( sym.sympify(interpolation_function).subs({sym.Symbol("x"):abs(coeff_mond.to(_CMOND_base_units["m"]/self._temp_profile_output_units).value*(((self.temp_profile/self.density_profile)*(drhodr_sym))+(dTdr_sym)))})))
+            self._has_dynamical_mass_profile_mond = True
+            self._dynamical_mass_profile_mond_units = output_units
+
+
+
+
+######################################
+#                                    #
+#       Function Definitions         #
+#                                    #
+######################################
+
+def convert_to_base_units(quantity:u.quantity.Quantity) -> u.quantity.Quantity:
+    """
+    Converts the input quantity to the units for the library.
+
+    Units are assigned by setting values in _CMOND_base_units variable
+    :param quantity: The quantity to convert to base units.
+    :return: Returns the converted value of the quantity.
+    """
+    log.debug("CMOND:Clusters:convert_to_base_units:DEBUG: Attempting to convert %s to base units."%quantity)
+    # getting string representation in base units
+    unit_string = str(quantity.decompose(bases=u.si.bases).unit) # Grabs the units of the quantity and creates a string
+
+    ### MANUAL EXCEPTIONS ###
+    if "kg m2 / s2" in unit_string:
+        ### The units are of energy, we convert to the energy unit
+        return quantity.to(_CMOND_base_units["E"])
+    # Unit conversion
+    for base_unit in _CMOND_base_units: # Iterate through each base unit
+        if base_unit in unit_string: # the unit is involved and needs to be replaced
+            unit_string = unit_string.replace(base_unit,str(_CMOND_base_units[base_unit]))
+            log.debug("CMOND:Clusters:convert_to_base_units:DEBUG: Unit string of %s is now %s."%(quantity,unit_string))
+
+    log.info("CMOND:Clusters:convert_to_base_units:INFO: Converted %s to %s"%(quantity,quantity.to(u.Unit(unit_string))))
+    return quantity.to(u.Unit(unit_string)) # returning the correct quantity after conversion of the string.
+
+def read_cluster_csv(filename:str,mode:str ="VIKH"):
+    """
+    Reads excel file with the given file name and generates clusters for each one.
+    :param filename: The file path to the given data set.
+    :param mode: The mode to use for the fits.
+    :return: Returns the list of generated clusters
+    """
+    ## Intro logging
+    log.info("CMOND:Clusters:read_cluster_csv:INFO: Attempting to construct clusters from %s."%filename)
+
+    ## Reading the file
+    if os.path.isfile(filename):
+        # The file exists
+        try:
+            dataframe = pd.read_csv(filename)
+        except Exception:
+            raise ReferenceError("CMOND:Clusters:read_cluster_csv:ERROR: Failed to read %s as a .csv."%filename)
+    else:
+        raise FileNotFoundError("CMOND:Clusters:read_cluster_csv:ERROR: Failed to find %s."%filename)
+
+
+    ### BUILDING THE CLUSTERS ###
+
+    CLUSTERS = list(dataframe["Cluster"]) # Reads the column of cluster names.
+
+    output_clusters = []
+    for cluster in CLUSTERS:
+        temp_cluster = Cluster(name=cluster) # Creates the cluster with the correct name
+
+        ### Building models
+        params = {i.split(",")[0]:dataframe.loc[dataframe["Cluster"]==cluster,i].item()*u.Unit(i.split(",")[1][1:-1]) for i in dataframe.columns[1:]}
+
+        # Building the temperature distribution
+        if mode=="VIKH":
+            # We are running in mode with Vikhlinin et al. 2006
+            try:
+                temp_cluster.density_fit_sym(params,"VIKH")
+
+            except Exception:
+                log.warning("CMOND:Clusters:read_cluster_CSV:WARNING: Failed to fit density model from mode: %s to the parameters for cluster %s."%(mode,cluster))
+            try:
+                temp_cluster.temp_fit_sym(params,"VIKH")
+            except Exception:
+                log.warning("CMOND:Clusters:read_cluster_CSV:WARNING: Failed to fit temperature model from mode: %s to the parameters for cluster %s."%(mode,cluster))
+            try:
+                temp_cluster.dynamical_mass_fit_sym(nu=_nu)
+            except Exception:
+                log.warning("CMOND:Clusters:read_cluster_CSV:WARNING: Failed to fit dynamical mass model from mode: %s to the parameters for cluster %s."%(mode,cluster))
+
+            try:
+                temp_cluster.dynamical_mass_fit_sym(nu=_nu,gravitational_mode="MOND")
+            except Exception:
+                log.warning("CMOND:Clusters:read_cluster_CSV:WARNING: Failed to fit dynamical (mond) mass model from mode: %s to the parameters for cluster %s."%(mode,cluster))
+
+            ### Adding other useful parameters
+            if "r_min" in params and not np.isnan(params["r_min"].value):
+                temp_cluster.r_min = params["r_min"]
+            if "r_500" in params and not np.isnan(params["r_500"].value):
+                temp_cluster.r_500 = params["r_500"]
+
+
+        ### Cleanup
+        if temp_cluster._has_temp_profile:
+            if temp_cluster.temp_profile == sym.nan:
+                temp_cluster.temp_profile = None
+                temp_cluster._has_temp_profile = False
+        if temp_cluster._has_density_profile:
+            if temp_cluster.density_profile == sym.nan:
+                temp_cluster._has_density_profile = False
+                temp_cluster.density_profile = None
+
+
+        output_clusters.append(temp_cluster)
+    return output_clusters
+
+
+if __name__ == '__main__':
+    log.basicConfig(level=log.WARNING)
+    clusters = read_cluster_csv("C:\\Users\\13852\\PycharmProjects\\CMOND\\Datasets\\Vikhlinin.csv")
+    for c in clusters:
+        c.dynamical_mass_fit_sym()
---
+++ Modules/Visualizations.py
@@ -0,0 +1,403 @@
+"""
+
+       Galaxy Cluster Visualizations
+        Written by: Eliza Diggins
+
+
+"""
+
+### Imports
+import matplotlib.pyplot as plt
+import numpy as np
+import astropy.units as u
+import logging as log
+import sympy as sym
+import Modules.Clusters as C
+
+### Functions
+
+def plot_temperature_profiles(clusters,
+                              r_min=None,
+                              r_max=None,
+                              n=500,
+                              indp_unit = u.kpc,
+                              dep_unit = u.keV,
+                              outer_scale_factor=1.0,
+                              scale="semilogx"
+                              ):
+    """
+    Plots the temperature profiles for the given clusters.
+    :param clusters: The list of cluster objects to plot.
+    :type clusters: list[Cluster]
+    :return:
+    """
+    log.info("CMOND:Visualizations:plot_temperature_profiles:INFO plotting temperature profiles for %s."%[cluster.name for cluster in clusters])
+
+    # Cleaning clusters
+    USABLE_CLUSTERS = [cluster for cluster in clusters if cluster._has_temp_profile]
+    log.debug("CMOND:Visualization:plot_temperature_profiles:DEBUG: Usable clusters are %s"%[cluster.name for cluster in USABLE_CLUSTERS])
+    
+    # Fixing bounds
+    if r_min == None:
+        r_min = min([c.r_min for c in USABLE_CLUSTERS if c.r_min != None])
+        CUSTOM_MIN=False
+    else:
+        CUSTOM_MIN = True
+    if r_max == None:
+        r_max = max([c.r_500 for c in USABLE_CLUSTERS if c.r_500 != None])
+        CUSTOM_MAX = False
+    else:
+        CUSTOM_MAX = True
+
+    r_min = r_min.to(indp_unit)
+    r_max = r_max.to(indp_unit)
+    
+    
+    # plotting
+    fig = plt.figure(figsize=(10,6))
+    ax1 = fig.add_subplot(111)
+
+    # Managing the scale
+
+    if scale == "semilogx":
+        ax1.set_xscale("log")
+    elif scale == "semilogy":
+        ax1.set_yscale("log")
+    elif scale == "loglog":
+        ax1.set_xscale("log")
+        ax1.set_yscale("log")
+    else:
+        pass
+
+
+    
+    for cluster in USABLE_CLUSTERS:
+        # Building r array
+        if not CUSTOM_MAX:
+            if cluster.r_500 != None:
+                ar_max = cluster.r_500
+            else:
+                ar_max = r_max
+        else:
+            ar_max = r_max
+        
+        if not CUSTOM_MIN:
+            if cluster.r_min != None:
+                ar_min = cluster.r_min
+            else:
+                ar_min = r_min
+        else:
+            ar_min = r_min
+
+        r_array = np.linspace(ar_min,ar_max,n)
+
+        output = sym.lambdify(sym.Symbol("r"),cluster.temp_profile,"numpy")((r_array).to(C._CMOND_base_units["m"]).value)
+        corrected_output = (output*cluster._temp_profile_output_units).to(dep_unit).value
+
+
+        ax1.plot(r_array,corrected_output  ,label=cluster.name)
+
+    ax1.set_xlabel("Radial Distance [%s]"%str(indp_unit))
+    ax1.set_ylabel("Gas Temperature [%s]"%str(dep_unit))
+    ax1.set_title("Gas Temperature Profiles")
+    plt.subplots_adjust(left=0.1,right=0.75)
+    plt.grid()
+    plt.legend(bbox_to_anchor=(1.05,1),title="Clusters")
+
+    plt.show()
+
+
+def plot_density_profiles(clusters,
+                              r_min=None,
+                              r_max=None,
+                              n=500,
+                              indp_unit=u.kpc,
+                              dep_unit=u.solMass/(u.m**3),
+                              outer_scale_factor=1.0,
+                              scale="semilogx"
+                              ):
+    """
+    Plots the temperature profiles for the given clusters.
+    :param clusters: The list of cluster objects to plot.
+    :type clusters: list[Cluster]
+    :return:
+    """
+    log.info(
+        "CMOND:Visualizations:plot_temperature_profiles:INFO plotting temperature profiles for %s." % [cluster.name for
+                                                                                                       cluster in
+                                                                                                       clusters])
+
+    # Cleaning clusters
+    USABLE_CLUSTERS = [cluster for cluster in clusters if cluster._has_density_profile]
+    log.debug(
+        "CMOND:Visualization:plot_temperature_profiles:DEBUG: Usable clusters are %s" % [cluster.name for cluster in
+                                                                                         USABLE_CLUSTERS])
+
+    # Fixing bounds
+    if r_min == None:
+        r_min = min([c.r_min for c in USABLE_CLUSTERS if c.r_min != None])
+        CUSTOM_MIN = False
+    else:
+        CUSTOM_MIN = True
+    if r_max == None:
+        r_max = max([c.r_500 for c in USABLE_CLUSTERS if c.r_500 != None])
+        CUSTOM_MAX = False
+    else:
+        CUSTOM_MAX = True
+
+    r_min = r_min.to(indp_unit)
+    r_max = r_max.to(indp_unit)
+
+    # plotting
+    fig = plt.figure(figsize=(10, 6))
+    ax1 = fig.add_subplot(111)
+
+    # Managing the scale
+
+    if scale == "semilogx":
+        ax1.set_xscale("log")
+    elif scale == "semilogy":
+        ax1.set_yscale("log")
+    elif scale == "loglog":
+        ax1.set_xscale("log")
+        ax1.set_yscale("log")
+    else:
+        pass
+
+    for cluster in USABLE_CLUSTERS:
+        # Building r array
+        if not CUSTOM_MAX:
+            if cluster.r_500 != None:
+                ar_max = cluster.r_500
+            else:
+                ar_max = r_max
+        else:
+            ar_max = r_max
+
+        if not CUSTOM_MIN:
+            if cluster.r_min != None:
+                ar_min = cluster.r_min
+            else:
+                ar_min = r_min
+        else:
+            ar_min = r_min
+
+        r_array = np.linspace(ar_min,ar_max,n)
+
+        output = sym.lambdify(sym.Symbol("r"), cluster.density_profile, "numpy")(
+            r_array.to(C._CMOND_base_units["m"]).value)
+        corrected_output = (output * cluster._density_profile_output_units).to(dep_unit).value
+
+
+        ax1.plot(r_array, corrected_output, label=cluster.name)
+
+    ax1.set_xlabel("Radial Distance [%s]" % str(indp_unit))
+    ax1.set_ylabel("Gas Density [%s]" % str(dep_unit))
+    ax1.set_title("Gas Density Profiles")
+    plt.subplots_adjust(left=0.1, right=0.75)
+    plt.grid()
+    plt.legend(bbox_to_anchor=(1.1, 1), title="Clusters")
+
+    plt.show()
+
+def plot_dynamical_mass_profiles(clusters,
+                              r_min=None,
+                              r_max=None,
+                              n=500,
+                              indp_unit=u.kpc,
+                              dep_unit=u.solMass,
+                              outer_scale_factor=1.0,
+                              scale="semilogx",
+                              mode="NEWT"
+                              ):
+    """
+    Plots the temperature profiles for the given clusters.
+    :param clusters: The list of cluster objects to plot.
+    :type clusters: list[Cluster]
+    :return:
+    """
+    log.info(
+        "CMOND:Visualizations:plot_temperature_profiles:INFO plotting temperature profiles for %s." % [cluster.name for
+                                                                                                       cluster in
+                                                                                                       clusters])
+
+    # Cleaning clusters
+    USABLE_CLUSTERS = [cluster for cluster in clusters if cluster._has_dynamical_mass_profile_newt]
+    log.debug(
+        "CMOND:Visualization:plot_temperature_profiles:DEBUG: Usable clusters are %s" % [cluster.name for cluster in
+                                                                                         USABLE_CLUSTERS])
+
+    # Fixing bounds
+    if r_min == None:
+        r_min = min([c.r_min for c in USABLE_CLUSTERS if c.r_min != None])
+        CUSTOM_MIN = False
+    else:
+        CUSTOM_MIN = True
+    if r_max == None:
+        r_max = max([c.r_500 for c in USABLE_CLUSTERS if c.r_500 != None])
+        CUSTOM_MAX = False
+    else:
+        CUSTOM_MAX = True
+
+    r_min = r_min.to(indp_unit)
+    r_max = r_max.to(indp_unit)
+
+    # plotting
+    fig = plt.figure(figsize=(10, 6))
+    ax1 = fig.add_subplot(111)
+
+    # Managing the scale
+
+    if scale == "semilogx":
+        ax1.set_xscale("log")
+    elif scale == "semilogy":
+        ax1.set_yscale("log")
+    elif scale == "loglog":
+        ax1.set_xscale("log")
+        ax1.set_yscale("log")
+    else:
+        pass
+
+    for cluster in USABLE_CLUSTERS:
+        # Building r array
+        if not CUSTOM_MAX:
+            if cluster.r_500 != None:
+                ar_max = cluster.r_500
+            else:
+                ar_max = r_max
+        else:
+            ar_max = r_max
+
+        if not CUSTOM_MIN:
+            if cluster.r_min != None:
+                ar_min = cluster.r_min
+            else:
+                ar_min = r_min
+        else:
+            ar_min = r_min
+
+        r_array = np.linspace(ar_min,ar_max,n)
+
+        if mode == "NEWT":
+            output = sym.lambdify(sym.Symbol("r"), cluster.dynamical_mass_profile_newt, "numpy")(
+            (r_array).to(C._CMOND_base_units["m"]).value)
+            corrected_output = (output * cluster._dynamical_mass_profile_newt_units).to(dep_unit).value
+        elif mode == "MOND":
+            output = sym.lambdify(sym.Symbol("r"), cluster.dynamical_mass_profile_mond, "numpy")(
+            (r_array).to(C._CMOND_base_units["m"]).value)
+            corrected_output = (output * cluster._dynamical_mass_profile_mond_units).to(dep_unit).value
+
+
+
+        ax1.plot(r_array, corrected_output, label=cluster.name)
+
+    ax1.set_xlabel("Radial Distance [%s]" % str(indp_unit))
+    ax1.set_ylabel(r"Dynamical Mass In Radius $r$ [%s]" % str(dep_unit))
+    if mode == "NEWT":
+        ax1.set_title("Newtonian Dynamical Mass Profiles")
+    elif mode == "MOND":
+        ax1.set_title("Milgromian Dynamical Mass Profiles")
+    plt.subplots_adjust(left=0.1, right=0.75)
+    plt.grid()
+    plt.legend(bbox_to_anchor=(1.1, 1), title="Clusters")
+
+    plt.show()
+
+def plot_mond_newt_profile(cluster,r_min=None,r_max=None,indp_unit=u.kpc,scale="loglog",dep_unit=u.solMass,n=1000):
+    """
+    Plots both the milgromian and newtonian profiles for the mass.
+    :param cluster: The cluster to plot
+    :return: None
+    """
+    log.info(
+        "CMOND:Visualizations:plot_mond_newt_profile:INFO plotting mond and newt profiles for %s." % cluster.name)
+
+    if not (cluster._has_dynamical_mass_profile_newt and cluster._has_dynamical_mass_profile_mond):
+        raise NotImplementedError("CMOND:Visualizations:plot_mond_newt_profile:ERROR: Failed to find necessary profiles for plotting.")
+
+    # Fixing bounds
+    if r_min == None:
+        try:
+            r_min = cluster.r_min
+        except Exception:
+            r_min = 0*u.kpc
+        CUSTOM_MIN = False
+    else:
+        CUSTOM_MIN = True
+    if r_max == None:
+        try:
+            r_max = cluster.r_500
+        except Exception:
+            r_max = 1000*u.kpc
+        CUSTOM_MAX = False
+    else:
+        CUSTOM_MAX = True
+
+    r_min = r_min.to(indp_unit)
+    r_max = r_max.to(indp_unit)
+
+    # plotting
+    fig = plt.figure(figsize=(10, 6))
+    ax1 = fig.add_subplot(111)
+
+    # Managing the scale
+
+    if scale == "semilogx":
+        ax1.set_xscale("log")
+    elif scale == "semilogy":
+        ax1.set_yscale("log")
+    elif scale == "loglog":
+        ax1.set_xscale("log")
+        ax1.set_yscale("log")
+    else:
+        pass
+
+    if not CUSTOM_MAX:
+        if cluster.r_500 != None:
+            ar_max = cluster.r_500
+        else:
+            ar_max = r_max
+    else:
+        ar_max = r_max
+    if not CUSTOM_MIN:
+        if cluster.r_min != None:
+            ar_min = cluster.r_min
+        else:
+            ar_min = r_min
+    else:
+        ar_min = r_min
+    r_array = np.linspace(ar_min, ar_max, n)
+
+    output_newt = sym.lambdify(sym.Symbol("r"), cluster.dynamical_mass_profile_newt, "numpy")(
+            (r_array).to(C._CMOND_base_units["m"]).value)
+    output_mond = sym.lambdify(sym.Symbol("r"), cluster.dynamical_mass_profile_mond, "numpy")(
+            (r_array).to(C._CMOND_base_units["m"]).value)
+    #output_test = sym.lambdify(sym.Symbol("r"), cluster.test_unit, "numpy")(
+    #        (r_array).to(C._CMOND_base_units["m"]).value)
+
+
+    corrected_output_newt = (output_newt * cluster._dynamical_mass_profile_newt_units).to(dep_unit).value
+    corrected_output_mond = (output_mond * cluster._dynamical_mass_profile_mond_units).to(dep_unit).value
+    #corrected_output_test = (output_test * cluster._dynamical_mass_profile_mond_units).to(dep_unit).value
+
+    #ax1.plot(r_array,corrected_output_test)
+    ax1.plot(r_array, corrected_output_mond, label="MOND")
+    ax1.plot(r_array,corrected_output_newt,label="NEWT")
+
+    ax1.set_xlabel("Radial Distance [%s]" % str(indp_unit))
+    ax1.set_ylabel(r"Dynamical Mass In Radius $r$ [%s]" % str(dep_unit))
+    ax1.set_title("Dynamical Masses of %s in MOND and Newtonian Paradigms"%cluster.name)
+    plt.subplots_adjust(left=0.1, right=0.75)
+    plt.grid()
+    plt.legend(bbox_to_anchor=(1.1, 1), title="Clusters")
+
+    plt.show()
+
+if __name__ == '__main__':
+    log.basicConfig(level=log.WARNING)
+    import Modules.Clusters as C
+
+    clusts = C.read_cluster_csv("C:\\Users\\13852\\PycharmProjects\\CMOND\\Datasets\\Vikhlinin.csv")
+    plot_dynamical_mass_profiles([clusts[0]])
+    plot_dynamical_mass_profiles([clusts[0]],mode="MOND")
+    plot_mond_newt_profile(clusts[0])
